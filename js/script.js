12308101546c15fdb30fdcf50e31eda623696a9f
mehraTusharRevclerx <100279723+mehraTusharRevclerx@users.noreply.github.com>
2022-05-19 05:06:43 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+$(() => {
+  const showData = $("#showData");
+  const showStatus = $("#showStatus");
+  const showXhr = $("#showXhr");
+  const btn = $("#btn");
+  btn.click(function () {
+    // LOAD METHOD
+    // showData.load("https://jsonplaceholder.typicode.com/posts/1");
+    // GET METHOD
+    // $.get(
+    //   "https://jsonplaceholder.typicode.com/posts/1",
+    //   function (data, status, xhr) {
+    //     showData.text(JSON.stringify(data));
+    //     showStatus.text(status);
+    //     showXhr.text(xhr.status);
+    //   }
+    // );
+    // POST METHOD
+    $.post(
+      "https://jsonplaceholder.typicode.com/posts",
+      {
+        title: "this is title",
+        userId: 1,
+        name: "tushar",
+        number: 8091974444,
+      },
+
+      function (data, status, xhr) {
+        showData.html(JSON.stringify(data));
+        showStatus.text(status);
+        showXhr.text(xhr.status);
+      }
+    );
+    // AJAX MEHTOD
+    // $.ajax("https://jsonplaceholder.typicode.com/posts/2", {
+    //   type: "GET",
+    //   dataType: "json", // type of response data
+    //   timeout: 500, // timeout milliseconds
+    //   success: function (data, status, xhr) {
+    //     // success callback function
+    //     showData.html("Success" + JSON.stringify(data));
+    //   },
+    //   error: function (jqXhr, textStatus, errorMessage) {
+    //     // error callback
+    //     showData.html("ERROR" + JSON.stringify(errorMessage));
+    //   },
+    // });
+
+    // POST METHOD
+    // $.ajax("https://jsonplaceholder.typicode.com/posts", {
+    //   type: "POST",
+    //   dataType: "json", // type of response data
+    //   // timeout: 500, // timeout milliseconds
+    //   data: {
+    //     title: "this is title",
+    //     userId: 1,
+    //     name: "tushar",
+    //     number: 8091974444,
+    //   },
+    //   success: function (data, status, xhr) {
+    //     // success callback function
+    //     showData.html("Success" + JSON.stringify(data));
+    //   },
+    //   error: function (jqXhr, textStatus, errorMessage) {
+    //     // error callback
+    //     showData.html("ERROR" + JSON.stringify(errorMessage));
+    //   },
+    // });
+  });
+});
